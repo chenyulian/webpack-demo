4d3c9a3ee367342988333c6abc5c13b6a6908fee
kinppink <648270560@qq.com>
2021-03-27 15:40:36 +0800

    添加webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       // Also generate a test.html
